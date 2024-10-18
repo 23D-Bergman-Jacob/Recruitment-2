328cd48345f873cbc7424a4e987ab0486ad8ffe1
Jacob Bergman <Jacob.Bergman@elev.ga.ntig.se>
2024-10-18 09:43:21 +0200

    Recruitment färdig

--- Recruitment/obj/Debug/net8.0/Recruitment.AssemblyInfo.cs
+++ Recruitment/obj/Debug/net8.0/Recruitment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recruitment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd945c5980faf815433924ae338088efb0b94aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recruitment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recruitment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
